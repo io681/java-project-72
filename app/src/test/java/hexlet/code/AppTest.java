c9442715125cf99252f519fd76a21e73b5693b6b
io681 <io-68@yandex.ru>
2024-02-28 16:42:01 +0300

    step 6, add one test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    Javalin app;
+
+    @BeforeEach
+    public final void setUp() throws IOException, SQLException {
+        app = App.getApp();
+    }
+
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/");
+            assertEquals(200, response.code(), "Not valid Code");
+            assertNotNull(response.body());
+            assertTrue(response.body().string().contains("Проверить"), "Page not found");
+        });
+    }
+}

2b346a8b95add80bd1d366d0cde70f63e5ab0a74
io681 <io-68@yandex.ru>
2024-02-29 19:31:15 +0300

    update step 6

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import hexlet.code.models.Url;
+import hexlet.code.repositories.UrlRepository;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import static hexlet.code.utils.TimestampFormatter.getCurrentTimeStamp;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +31,46 @@ public class AppTest {
             assertEquals(200, response.code(), "Not valid Code");
             assertNotNull(response.body());
             assertTrue(response.body().string().contains("Проверить"), "Page not found");
+        });
+    }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls");
+            assertEquals(200, response.code(), "Not valid Code");
+            assertNotNull(response.body());
+            assertTrue(response.body().string().contains("Сайты"), "Page not found");
+        });
+    }
+
+    @Test
+    public void testCreateUrl() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "https://getbootstrap.com/docs/5.3/examples/";
+            var response = client.post("/urls", requestBody);
+            assertEquals(200, response.code(), "Not valid Code");
+        });
+    }
+
+
+    @Test
+    public void testUrlPage() throws SQLException {
+        var url = new Url("https://docs.oracle.com/en/java/javase/20/docs/api/java.base/java/net/URI.html#toURL()",
+                getCurrentTimeStamp());
+        UrlRepository.save(url);
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/" + url.getId());
+            assertTrue(response.body().string().contains("https://docs.oracle.com"), "Page not found");
+        });
+    }
+
+    @Test
+    public void testNotValidUrlPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "yaya";
+            var responseByPage = client.get("/urls");
+            assertFalse(responseByPage.body().string().contains("yaya"), "Bad Url created");
         });
     }
 }
